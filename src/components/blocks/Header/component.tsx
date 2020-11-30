589638942ec828afa37bff761c9b36b4e190170c
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-11-30 10:03:41 +0300

    fix bug with WorkModal

--- src/components/blocks/Header/component.tsx
+++ src/components/blocks/Header/component.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useCallback, useState } from 'react';
 import clsx from 'clsx';
 import {
   AppBar,
@@ -16,79 +16,94 @@ import Menu from './components/Menu';
 import WorkWidget from './components/WorkWidget';
 import AccountMenu from './components/AccountMenu';
 import InputText from 'components/controls/InputText';
+import WorkModal from './components/WorkWidget/components/WorkModal';
 import { useClasses } from './styles';
 
 const Header = (): ReactElement => {
   const classes = useClasses();
 
-  return (
-    <Grid className={classes.header}>
-      <AppBar position="static" className={classes.topBar}>
-        <Toolbar className={classes.toolBar}>
-          <Grid container classes={{ root: classes.gridContainer }}>
-            <Typography variant="h6" color="primary">
-              Mint art
-            </Typography>
-            <Menu />
-          </Grid>
-          <Grid container classes={{ root: classes.gridContainer }}>
-            <div className={classes.search}>
-              <InputText
-                placeholder="Search…"
-                icon={
-                  <InputAdornment position="start">
-                    <Search />
-                  </InputAdornment>
-                }
-                className={classes.input}
-              />
-            </div>
-            <Tooltip
-              title={<WorkWidget />}
-              interactive
-              arrow
-              classes={{
-                tooltip: clsx(classes.widget, classes.workWidget),
-                arrow: classes.widgetArrow,
-                tooltipPlacementBottom: classes.widgetMargin,
-              }}
-            >
-              <IconButton className={classes.icon}>
-                <Badge
-                  className={classes.badge}
-                  badgeContent="×"
-                  overlap="circle"
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                  }}
-                >
-                  <Work />
-                </Badge>
-              </IconButton>
-            </Tooltip>
-            <Tooltip
-              title={<AccountMenu />}
-              interactive
-              arrow
-              classes={{
-                tooltip: clsx(classes.widget, classes.accountWidget),
-                arrow: classes.widgetArrow,
-                tooltipPlacementBottom: classes.widgetMargin,
-              }}
-            >
-              <IconButton className={clsx(classes.icon, classes.account)}>
-                <AccountCircle />
-              </IconButton>
-            </Tooltip>
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-            <Button color="primary" variant="contained" disableElevation>
-              Upload
-            </Button>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </Grid>
+  const handleClickOpenModal = useCallback(() => {
+    setIsOpenModal(true);
+  }, [isOpenModal]);
+
+  const handleCloseModal = useCallback(() => {
+    setIsOpenModal(false);
+  }, [isOpenModal]);
+
+  return (
+    <>
+      <Grid className={classes.header}>
+        <AppBar position="static" className={classes.topBar}>
+          <Toolbar className={classes.toolBar}>
+            <Grid container classes={{ root: classes.gridContainer }}>
+              <Typography variant="h6" color="primary">
+                Mint art
+              </Typography>
+              <Menu />
+            </Grid>
+            <Grid container classes={{ root: classes.gridContainer }}>
+              <div className={classes.search}>
+                <InputText
+                  placeholder="Search…"
+                  icon={
+                    <InputAdornment position="start">
+                      <Search />
+                    </InputAdornment>
+                  }
+                  className={classes.input}
+                />
+              </div>
+              <Tooltip
+                title={<WorkWidget openModal={handleClickOpenModal} />}
+                interactive
+                arrow
+                classes={{
+                  tooltip: clsx(classes.widget, classes.workWidget),
+                  arrow: classes.widgetArrow,
+                  tooltipPlacementBottom: classes.widgetMargin,
+                }}
+              >
+                <IconButton className={classes.icon}>
+                  <Badge
+                    className={classes.badge}
+                    badgeContent="×"
+                    overlap="circle"
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'right',
+                    }}
+                  >
+                    <Work />
+                  </Badge>
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                title={<AccountMenu />}
+                interactive
+                arrow
+                classes={{
+                  tooltip: clsx(classes.widget, classes.accountWidget),
+                  arrow: classes.widgetArrow,
+                  tooltipPlacementBottom: classes.widgetMargin,
+                }}
+              >
+                <IconButton className={clsx(classes.icon, classes.account)}>
+                  <AccountCircle />
+                </IconButton>
+              </Tooltip>
+
+              <Button color="primary" variant="contained" disableElevation>
+                Upload
+              </Button>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+
+      {isOpenModal && <WorkModal isOpen={isOpenModal} close={handleCloseModal} />}
+    </>
   );
 };
 
--- src/components/blocks/Header/components/WorkWidget/component.tsx
+++ src/components/blocks/Header/components/WorkWidget/component.tsx
@@ -1,31 +1,19 @@
-import React, { ReactElement, useCallback, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { Divider, Paper, Typography } from '@material-ui/core';
 import WorkSwitcher from './components/WorkSwitcher';
-import WorkModal from './components/WorkModal';
+import { WorkWidgetProps } from './types';
 import { useClasses } from './styles';
 
-const WorkWidget = (): ReactElement => {
+const WorkWidget = ({ openModal }: WorkWidgetProps): ReactElement => {
   const classes = useClasses();
-
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
-  const handleClickOpenModal = useCallback(() => {
-    setIsOpenModal(true);
-  }, [isOpenModal]);
-
-  const handleCloseModal = useCallback(() => {
-    setIsOpenModal(false);
-  }, [isOpenModal]);
 
   return (
     <Paper elevation={0}>
       <WorkSwitcher />
       <Divider className={classes.divider} />
-      <Typography variant="subtitle2" className={classes.link} onClick={handleClickOpenModal}>
+      <Typography variant="subtitle2" className={classes.link} onClick={openModal}>
         Edit work availability
       </Typography>
-
-      <WorkModal isOpen={isOpenModal} close={handleCloseModal} />
     </Paper>
   );
 };
